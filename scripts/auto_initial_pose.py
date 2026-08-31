8e4f35b110405aa050c4d3f47a36b89dee626feb
Satvik Kumar <82556122+xEscapeVelocity@users.noreply.github.com>
2026-08-31 10:40:04 +0530

    Fix AMCL motion model plugin name in nav2_params.yaml

--- scripts/auto_initial_pose.py
+++ scripts/auto_initial_pose.py
@@ -57,7 +57,8 @@ def main(args=None):
         pass
     finally:
         node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
